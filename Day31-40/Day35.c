10a2ccdb4ff18e000c7593743f3f9239bc4d99a3
Krishna Kashyap <krishnakashyap.cse@gmail.com>
2026-03-07 10:00:00 +0530

    Day-35 code

--- Day31-40/Day35.c
+++ Day31-40/Day35.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #define MAX 100
-
 int queue[MAX];
 int front = -1;
 int rear = -1;
-
 void enqueue(int value)
 {
     if (rear == MAX - 1)
@@ -12,14 +10,11 @@ void enqueue(int value)
         printf("Queue Overflow\n");
         return;
     }
-
     if (front == -1)
         front = 0;
-
     rear++;
     queue[rear] = value;
 }
-
 void display()
 {
     if (front == -1)
@@ -27,31 +22,22 @@ void display()
         printf("Queue is empty\n");
         return;
     }
-
     printf("Queue elements (front to rear): ");
-
     for (int i = front; i <= rear; i++)
         printf("%d ", queue[i]);
-
     printf("\n");
 }
-
 int main()
 {
     int n, value;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     printf("Enter %d elements:\n", n);
-
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &value);
         enqueue(value);
     }
-
     display();
-
     return 0;
 }
